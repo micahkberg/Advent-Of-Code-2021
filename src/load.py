db217287614d1b8ff63b2b82921a0a4b2ed99082
nmpcgithub <nmpcgithub@outlook.com>
2022-12-06 14:13:48 -0800

    made load generic

--- src/load.py
+++ src/load.py
@@ -2,7 +2,7 @@ import os
 
 
 def openfile(filename):
-    path = 'C://Users/bobth/Documents/programming/Advent-Of-Code-2021/inputs'
+    path = '../inputs'
     os.chdir(path)
     with open(filename, 'r') as f:
         lines = f.read().split("\n")
